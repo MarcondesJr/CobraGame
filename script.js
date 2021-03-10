974ba773b6f854038a3e57b9705594ccdcf52b24
MarcondesJr <marcondestecinfo@gmail.com>
2021-03-09 20:02:01 -0400

    criando movimento

--- script.js
+++ script.js
@@ -6,18 +6,40 @@ cobra[0] = {
     x = 8 * box,
     y = 8 * box
 }
+let direction = "right";
 
 function criarBOX() {
-    context.fillStyle = "black";
+    context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobra() {
     for(i=0; i < cobra.length; i++){
-        context.fillStyle = "pink";
+        context.fillStyle = "green";
         context.fillRect(cobra[i].x, cobra[i].y, box, box);
     }
 }
 
-criarBOX();
-criarCobra();
+function inicioGame() {
+    criarBOX();
+    criarCobra();
+
+    let cobraX = cobra[0].x;
+    let cobraY = cobra[0].y;
+
+    if(direction == "right") cobraX += box;
+    if(direction == "left") cobraX -= box;
+    if(direction == "up") cobraY -= box;
+    if(direction == "down") cobraY += box;
+
+    cobra.pop();
+    
+    let novaHead = {
+        x: cobraX,
+        y: cobraY
+    }
+
+    cobra.unshift(novaHead);
+}
+
+let game = setInterval(inicioGame, 100);
