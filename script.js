0a98ba5586186f1680df777786878f2856ff0d1c
MarcondesJr <marcondestecinfo@gmail.com>
2021-03-09 21:38:40 -0400

    criando direcoes

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let context = canvas.getContext("2d");
 let box = 32;
 let cobra = [];
 cobra[0] = {
-    x = 8 * box,
-    y = 8 * box
+    x: 8 * box,
+    y: 8 * box
 }
 let direction = "right";
 
@@ -15,12 +15,26 @@ function criarBOX() {
 
 function criarCobra() {
     for(i=0; i < cobra.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = 'green';
         context.fillRect(cobra[i].x, cobra[i].y, box, box);
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function inicioGame() {
+    if(cobra[0].x > 15 * box && direction == "right") cobra[0].x = 0;
+    if(cobra[0].x < 0 && direction == "left") cobra[0].x = 16 * box;
+    if(cobra[0].y > 15 * box && direction == "down") cobra[0].y = 0;
+    if(cobra[0].y < 0 && direction == "up") cobra[0].y = 16 * box;
+
     criarBOX();
     criarCobra();
 
